bed1f3469c0abd628859bd7f3c65db5753dddae8
CmcvOSU <mcverryc@oregonstate.edu>
2021-01-11 11:10:06 -0500

    added addition_example.py

---
+++ addition_example.py
@@ -0,0 +1,13 @@
+
+
+def addition(x, y):
+    """adds two numbers received as parameters and returns result"""
+
+    result = x + y
+    return result
+
+def main():
+    if __name__ == "__main__":
+        print(addition(4, 5))
+
+main()
