a2d7920674bb0b1cb5171eab2a3c9f0335a8f123
CmcvOSU <mcverryc@oregonstate.edu>
2021-01-11 11:36:04 -0500

    added subtraction_example.py

---
+++ subtraction_example.py
@@ -0,0 +1,12 @@
+
+def subtraction(x, y):
+    """subtracts y from x received as parameters and returns result"""
+
+    result = x - y
+    return result
+
+def main():
+    if __name__ == "__main__":
+        print(subtraction(10, 6))
+
+main()
