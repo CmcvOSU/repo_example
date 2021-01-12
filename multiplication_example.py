c33df175ae734329d6f977c7c8ad683a668f3628
CmcvOSU <mcverryc@oregonstate.edu>
2021-01-11 22:47:40 -0500

    added multiplication_example

---
+++ multiplication_example.py
@@ -0,0 +1,11 @@
+def multiplication(x, y):
+    """Multiplies x and y received as parameters and returns result"""
+
+    result = x * y
+    return result
+
+def main():
+    if __name__ == "__main__":
+        print(multiplication(10, 6))
+
+main()
